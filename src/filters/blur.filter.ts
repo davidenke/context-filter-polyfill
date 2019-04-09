e0665a60a267da2919345fa3a9ff3b1708ba465b
David Enke <post@davidenke.de>
2019-04-09 19:48:27 +0200

    beautified blur but it's slow and not precise

--- src/filters/blur.filter.ts
+++ src/filters/blur.filter.ts
@@ -5,48 +5,48 @@ export const blur: Filter = (imageData, radius = '0') => {
   const amount = normalizeLength(radius);
   const { data, width } = imageData;
   const { length } = data;
-  console.log('blur', amount);
+  console.log('blur', amount, width);
 
-  // https://codepen.io/zhaojun/pen/zZmRQe
-  // https://gist.github.com/6174/9403745
-  // TODO: tweak, as it is too blurry
   // https://www.script-tutorials.com/html5-canvas-image-effects-app-adding-blur/
-  const p1 = 0.99;
-  const p2 = 0.99;
-  const p3 = 0.99;
-  const er = 0; // extra red
-  const eg = 0; // extra green
-  const eb = 0; // extra blue
+  const rgba = 4;
+  const rgbaWidth = rgba * width;
 
-  for (let rate = 0; rate < amount; rate += 1) {
-    for (let i = 0, n = length; i < n; i += 4) {
-      const iMW = 4 * width;
-      let iSumOpacity = 0;
-      let iSumRed = 0;
-      let iSumGreen = 0;
-      let iSumBlue = 0;
-      let iCnt = 0;
+  for (let rate = 0; rate < amount; rate++) {
+    for (let i = 0; i < length; i += rgba) {
+      let r = 0;
+      let g = 0;
+      let b = 0;
+      let a = 0;
+      let count = 0;
       // data of close pixels (from all 8 surrounding pixels)
-      const aCloseData = [
-        i - iMW - 4, i - iMW, i - iMW + 4, // top pixels
-        i - 4, i + 4, // middle pixels
-        i + iMW - 4, i + iMW, i + iMW + 4 // bottom pixels
+      const closePixels = [
+        // top pixels
+        i - rgbaWidth - rgba,
+        i - rgbaWidth,
+        i - rgbaWidth + rgba,
+        // center pixels
+        i - rgba,
+        i + rgba,
+        // bottom pixels
+        i + rgbaWidth - rgba,
+        i + rgbaWidth,
+        i + rgbaWidth + rgba
       ];
-      // calculating Sum value of all close pixels
-      for (let e = 0; e < aCloseData.length; e += 1) {
-        if (aCloseData[e] >= 0 && aCloseData[e] <= length - 3) {
-          iSumOpacity += data[aCloseData[e]];
-          iSumRed += data[aCloseData[e] + 1];
-          iSumGreen += data[aCloseData[e] + 2];
-          iSumBlue += data[aCloseData[e] + 3];
-          iCnt += 1;
+      // calculating sum value of all close pixels
+      for (let e = 0; e < closePixels.length; e++) {
+        if (closePixels[e] >= 0 && closePixels[e] <= length - 3) {
+          a += data[closePixels[e] + 0];
+          r += data[closePixels[e] + 1];
+          g += data[closePixels[e] + 2];
+          b += data[closePixels[e] + 3];
+          count += 1;
         }
       }
       // apply average values
-      data[i] = (iSumOpacity / iCnt)*p1+er;
-      data[i+1] = (iSumRed / iCnt)*p2+eg;
-      data[i+2] = (iSumGreen / iCnt)*p3+eb;
-      data[i+3] = (iSumBlue / iCnt);
+      data[i + 0] = a / count;
+      data[i + 1] = r / count;
+      data[i + 2] = g / count;
+      data[i + 3] = b / count;
     }
   }
 
