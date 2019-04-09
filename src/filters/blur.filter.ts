74a19fb020d61d0b840b8d2457f8b6f3dc204876
David Enke <post@davidenke.de>
2019-04-09 18:13:44 +0200

    added better blur filter still needs to be adjusted

--- src/filters/blur.filter.ts
+++ src/filters/blur.filter.ts
@@ -1,28 +1,54 @@
 import { Filter } from '../types/filter.type';
 import { normalizeLength } from '../utils/filter.utils';
 
-export const blur: Filter = (context, radius = '0') => {
+export const blur: Filter = (imageData, radius = '0') => {
   const amount = normalizeLength(radius);
+  const { data, width } = imageData;
+  const { length } = data;
   console.log('blur', amount);
 
-  // TODO: tweak, as it is too blurry
   // https://codepen.io/zhaojun/pen/zZmRQe
   // https://gist.github.com/6174/9403745
-  let sum = 0;
-  let delta = 5;
-  let alpha_left = 1 / (2 * Math.PI * delta * delta);
-  let step = amount < 3 ? 1 : 2;
-  for (let y = -amount; y <= amount; y += step) {
-    for (let x = -amount; x <= amount; x += step) {
-      let weight = alpha_left * Math.exp(-(x * x + y * y) / (2 * delta * delta));
-      sum += weight;
+  // TODO: tweak, as it is too blurry
+  // https://www.script-tutorials.com/html5-canvas-image-effects-app-adding-blur/
+  const p1 = 0.99;
+  const p2 = 0.99;
+  const p3 = 0.99;
+  const er = 0; // extra red
+  const eg = 0; // extra green
+  const eb = 0; // extra blue
+
+  for (let rate = 0; rate < amount; rate += 1) {
+    for (let i = 0, n = length; i < n; i += 4) {
+      const iMW = 4 * width;
+      let iSumOpacity = 0;
+      let iSumRed = 0;
+      let iSumGreen = 0;
+      let iSumBlue = 0;
+      let iCnt = 0;
+      // data of close pixels (from all 8 surrounding pixels)
+      const aCloseData = [
+        i - iMW - 4, i - iMW, i - iMW + 4, // top pixels
+        i - 4, i + 4, // middle pixels
+        i + iMW - 4, i + iMW, i + iMW + 4 // bottom pixels
+      ];
+      // calculating Sum value of all close pixels
+      for (let e = 0; e < aCloseData.length; e += 1) {
+        if (aCloseData[e] >= 0 && aCloseData[e] <= length - 3) {
+          iSumOpacity += data[aCloseData[e]];
+          iSumRed += data[aCloseData[e] + 1];
+          iSumGreen += data[aCloseData[e] + 2];
+          iSumBlue += data[aCloseData[e] + 3];
+          iCnt += 1;
+        }
+      }
+      // apply average values
+      data[i] = (iSumOpacity / iCnt)*p1+er;
+      data[i+1] = (iSumRed / iCnt)*p2+eg;
+      data[i+2] = (iSumGreen / iCnt)*p3+eb;
+      data[i+3] = (iSumBlue / iCnt);
     }
   }
-  for (let y = -amount; y <= amount; y += step) {
-    for (let x = -amount; x <= amount; x += step) {
-      context.globalAlpha = alpha_left * Math.exp(-(x * x + y * y) / (2 * delta * delta)) / sum * amount;
-      context.drawImage(context.canvas, x, y);
-    }
-  }
-  context.globalAlpha = 1;
+
+  return imageData;
 };
