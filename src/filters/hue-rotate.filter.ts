fd4d2eeb26d367aed64bf4b4997701455a9a0f65
David Enke <post@davidenke.de>
2019-04-11 01:04:14 +0200

    added hue-rotate filter but it needs improvements for stauration

---
+++ src/filters/hue-rotate.filter.ts
@@ -0,0 +1,82 @@
+import { Filter } from '../types/filter.type';
+import { normalizeAngle } from '../utils/filter.utils';
+
+export const hueRotate: Filter = (imageData, rotate = '0deg') => {
+  let amount = normalizeAngle(rotate);
+  console.log('hue-rotate', amount);
+
+  // do not manipulate without proper amount
+  if (amount <= 0) {
+    return imageData;
+  }
+
+  const { data, height, width } = imageData;
+
+  // in rgba world, every
+  // n * 4 is red,
+  // n * 4 + 1 green and
+  // n * 4 + 2 is blue
+  // the fourth can be skipped as it's the alpha channel
+  // https://github.com/makoConstruct/canvas-hue-rotate/blob/master/hueShiftCanvas.js
+  const h = (amount % 1 + 1) % 1; // wraps the angle to unit interval, even when negative
+  const th = h * 3;
+  const thr = Math.floor(th);
+  const d = th - thr;
+  const b = 1 - d;
+  let ma, mb, mc;
+  let md, me, mf;
+  let mg, mh, mi;
+
+  switch (thr) {
+    case 0:
+      ma = b;
+      mb = 0;
+      mc = d;
+      md = d;
+      me = b;
+      mf = 0;
+      mg = 0;
+      mh = d;
+      mi = b;
+      break;
+    case 1:
+      ma = 0;
+      mb = d;
+      mc = b;
+      md = b;
+      me = 0;
+      mf = d;
+      mg = d;
+      mh = b;
+      mi = 0;
+      break;
+    case 2:
+      ma = d;
+      mb = b;
+      mc = 0;
+      md = 0;
+      me = d;
+      mf = b;
+      mg = b;
+      mh = 0;
+      mi = d;
+      break;
+  }
+  //do the pixels
+  let place = 0;
+  for (let y = 0; y < height; ++y) {
+    for (let x = 0; x < width; ++x) {
+      place = 4 * (y * width + x);
+
+      const ir = data[place + 0];
+      const ig = data[place + 1];
+      const ib = data[place + 2];
+
+      data[place + 0] = Math.floor(ma * ir + mb * ig + mc * ib);
+      data[place + 1] = Math.floor(md * ir + me * ig + mf * ib);
+      data[place + 2] = Math.floor(mg * ir + mh * ig + mi * ib);
+    }
+  }
+
+  return imageData;
+};
